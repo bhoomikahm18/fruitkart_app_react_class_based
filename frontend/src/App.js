8569bffbc928eaec162f32259f8d3468bd9ed5f4
Bhoomika <bhoomikahm18@gmail.com>
2023-11-10 16:43:22 +0530

    Calling products and cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Products from './components/products/Products.jsx';
+import Signup from './components/signup/Signup.jsx';
+import Cart from './components/cart/Cart.jsx';
+import Header from './components/header/Header.jsx';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <h1>Hello world</h1>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Header cartItems={null} />
+          <Routes>
+            <Route exact path="/" element={<Products />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/cart" element={<Cart />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
