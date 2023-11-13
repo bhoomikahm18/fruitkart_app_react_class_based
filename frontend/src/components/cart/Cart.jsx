8600510e79a017557d84fc75d7297b788099237e
Bhoomika <bhoomikahm18@gmail.com>
2023-11-13 12:19:53 +0530

    Adding cart components

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react';
+import './Cart.css';
 
-function Cart() {
-  return (
-    <div>Cart</div>
-  )
+export default class Cart extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+
+
+    let totalPrice = this.props.cartItems.reduce((cost, item) => {
+      return cost + (item.quantity * item.price)
+    }, 0);
+
+
+    return (
+      <div className='cart-items'>
+        <h2 className='cart-items-header'>Cart Items</h2>
+        <div className='clear-cart'>
+          {this.props.cartItems.length >= 1 && (
+            <button className='clear-cart-button' onClick={this.props.handleCartClearence}>Clear Cart</button>
+          )}
+        </div>
+        {this.props.cartItems.length === 0 &&
+          <div className='cart-items-empty'>
+            No items are added in the cart
+          </div>}
+        <div>
+
+          {this.props.cartItems.map(item => {
+            return (<div key={item.id} className="cart-items-list">
+              <img className="cart-item-image"
+                src={item.image}
+                alt={item.name} />
+              <div className='cart-items-name'>
+                {item.name}
+              </div>
+              <div className='cart-items-function'>
+                <button className='cart-items-add' onClick={() => this.props.handleAddProduct(item)}>+</button>
+                <button className='cart-items-remove' onClick={() => this.props.handleRemoveProduct(item)}>-</button>
+              </div>
+              <div className='cart-items-price'>
+                {item.quantity} x Rs.{item.price}
+              </div>
+            </div>
+            );
+          })
+          }
+        </div>
+        <div className='cart-items-total-price-name'>
+          Total Price
+          <div className='cart-items-total-price'> Rs.{totalPrice}</div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default Cart
